43819df8ddf31120c9edc448364f78062b0a16c5
Charly Barrios <111667268+charlynbc@users.noreply.github.com>
2025-09-11 12:46:22 +0000

    main basico

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String nombre = "Flow";
+        System.out.println("Bienvenido a la peluqueria " + nombre   );
+        // Aquí puedes iniciar la lógica principal de la aplicación
+    }
+}
