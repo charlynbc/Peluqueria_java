8b6260d487affc589a29c3b3d0502901039c0377
Charly Barrios <111667268+charlynbc@users.noreply.github.com>
2025-09-11 12:37:03 +0000

    Primera clase, el cliente actualizaion 1

---
+++ src/Cliente.java
@@ -0,0 +1,26 @@
+public class Cliente{
+
+    String email;
+    String nombre;
+    int edad;
+
+    public Cliente(String email, String nombre, int edad){
+        this.email= email;
+        this.nombre= nombre;
+        this.edad= edad;
+    }
+    //Getters para leer los valores
+    public String get_nombre(){
+        return nombre;
+    } 
+    public String get_email(){
+        return email;
+    }
+    public int get_edad(){
+        return edad;
+    }
+    //setters
+    public void set_nombre(String nombre){ this.nombre= nombre;}
+    public void set_edad(int edad){this.edad= edad}
+
+}
